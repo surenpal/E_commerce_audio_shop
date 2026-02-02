5d96e6c6df3e557dcdda19e5c1e7cc83f1cd22d4
suren <surenpal666@gmail.com>
2026-02-02 22:11:09 +0900

    react router dom is install for the paging

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {BrowserRouter, Router, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (

9844eed9348f2e75da6b05b01940a727939784f1
suren <surenpal666@gmail.com>
2026-02-02 21:01:47 +0900

    first e-commerce project by suren pal:journey to the full stack developer begins here

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+
+
+      <h1  className='bg-red-500 text-white shadow-lg p-4 rounded-lg'>Suren Pal</h1>
+    
+    </div>
+  )
+}
+
+export default App

c59d76f520c6a0e945983de8c766c10ae16bdd29
suren <surenpal666@gmail.com>
2026-02-04 23:56:41 +0900

    icon css added for the facareddown

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,8 +10,8 @@ const Navbar = () => {
     <div className='bg-white py-3 shadow-2xl'>
       
       <div className='max-w-6xl mx-auto flex justify-between items-center'>
-        <div className='text-2xl font-bold'>
-          
+        <div className='flex items-center gap-8'>
+
           <Link to="/" className='font-bold text-3xl'><span className='text-blue-500 font-serif'>B</span>a<span  className= "text-red-500 font-serif">s</span>s</Link>
            <div className='flex gap-1 cursor-pointer text-gray-700 items-center'>
             <MapPin className='text-red-500'/>

9dbf14586c9d917d571a1b37bcd01957f5a2ceb6
suren <surenpal666@gmail.com>
2026-02-08 01:23:55 +0900

    IotCartOutline is imported

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,18 +2,23 @@ import React from 'react'
 import { Link , NavLink} from 'react-router-dom';
 import { MapPin } from 'lucide-react';
 import {FaCaretDown} from 'react-icons/fa6';
+import {IoCartOutline} from 'react-icons/io5';
 
 
 const Navbar = () => {
   const location = false;; 
   return (
     <div className='bg-white py-3 shadow-2xl'>
-      
-      <div className='max-w-6xl mx-auto flex justify-between items-center'>
-       
+      <div className='max-w-6xl flex justify-between items-center'>
         <div className='flex gap-7 items-center'>
+          <Link to={"/"}>
+          <h1 className='font-bold text-3xl text-pink-600'>
+            <span className='text-blue-500 font-serif'>B</span>
+            a
+            <span  className= "text-red-500 font-serif">s</span>
+            s
+            </h1></Link>
 
-          <Link to="/" className='font-bold text-3xl'><span className='text-blue-500 font-serif'>B</span>a<span  className= "text-red-500 font-serif">s</span>s</Link>
            <div className='flex gap-1 cursor-pointer text-gray-700 items-center'>
             <MapPin className='text-red-500'/>
             <span className="font-semibold">
@@ -25,16 +30,19 @@ const Navbar = () => {
 
 
         <nav>
-          <ul className='flex gap-7 text-xl font-semibold'>
-            <NavLink to="/" className={({isActive}) => isActive ? 'border-b-4 border-red-500' : ''}>Home</NavLink>
-            <NavLink to="/products" className={({isActive}) => isActive ? 'border-b-4 border-red-500' : ''}>Products</NavLink>
-            <NavLink to="/about" className={({isActive}) => isActive ? 'border-b-4 border-red-500' : ''}>About</NavLink>
-            <NavLink to="/contact" className={({isActive}) => isActive ? 'border-b-4 border-red-500' : ''}>Contact</NavLink>
+          <ul className='flex gap-7 items-center text-xl font-semibold'>
+            <NavLink to={"/"} className={({isActive}) => isActive ? 'border-b-4 border-red-500' : ''}>Home</NavLink>
+            <NavLink to={"/products"} className={({isActive}) => isActive ? 'border-b-4 border-red-500' : ''}>Products</NavLink>
+            <NavLink to={"/about"} className={({isActive}) => isActive ? 'border-b-4 border-red-500' : ''}>About</NavLink>
+            <NavLink to={"/contact"} className={({isActive}) => isActive ? 'border-b-4 border-red-500' : ''}>Contact</NavLink>
           </ul>
+          <Link to= {"/cart"} >
+          <IoCartOutline className= 'h-7 w-7'/>
+          <span>0</span></Link>
         </nav>
 
       </div> 
-    </div>
+    </div>   
   )
 }
 

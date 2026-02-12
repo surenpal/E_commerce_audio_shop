c4e3d1fed1f1be63e887fec8088c78cd3170caff
suren <surenpal666@gmail.com>
2026-02-13 00:07:50 +0900

    product appi si adde d biut under the chscking hrer

--- src/context/DataContext.jsx
+++ src/context/DataContext.jsx
@@ -6,6 +6,22 @@ export const DataContext = createContext(null);
 
 export const DataProvider = ({children}) => {
     const [data, setData] = useState()
+
+    //here we can define functions to fetch or manipulate data
+
+    const fetchAllProducts = async () => {
+        try {
+            const response = await axios('https://fakestoreapi.in/api/products?limit=150');
+            const products = await response.json();
+            setData(products);
+        }
+        catch (error) {
+            console.error("Error fetching products:", error);
+        }
+    }
+
+
+
     return <DataContext.Provider value= {{data, setData}}>
         {children}
     </DataContext.Provider>

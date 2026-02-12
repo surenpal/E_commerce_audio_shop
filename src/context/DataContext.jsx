d4c0db78300df10418e806c3b8fe91e135610a56
suren <surenpal666@gmail.com>
2026-02-12 23:52:26 +0900

    first the dataContext is created to excess all the shared data without proper drilling

--- src/context/DataContext.jsx
+++ src/context/DataContext.jsx
@@ -1,4 +1,12 @@
 import { createContext } from "react";
+import { useState } from "react";
+
 
 export const DataContext = createContext(null);
 
+export const DataProvider = ({children}) => {
+    const [data, setData] = useState()
+    return <DataContext.Provider value= {{data, setData}}>
+        {children}
+    </DataContext.Provider>
+}

e128bf0c8555050b8a9bf04fe5cda255d56072cf
suren <surenpal666@gmail.com>
2026-02-17 20:59:58 +0900

    slider is also imported from react slick

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -2,6 +2,7 @@ import React, { use, useContext, useEffect } from 'react'
 import { DataContext } from '../context/DataContext'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 const Carousel = () => {
 
@@ -12,8 +13,37 @@ const Carousel = () => {
     fetchAllProducts()
   }, [])
 
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  }
+
   return (
-    <div> This is the Carousel</div>
+    <div>
+      <Slider {...settings}>
+        <div>
+          <h3>1</h3>
+        </div>
+        <div>
+          <h3>2</h3>
+        </div>
+        <div>
+          <h3>3</h3>
+        </div>
+        <div>
+          <h3>4</h3>
+        </div>
+        <div>
+          <h3>5</h3>
+        </div>
+        <div>
+          <h3>6</h3>
+        </div>
+      </Slider>
+    </div>
   )
 }
 

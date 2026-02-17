6f0d2ffe7608e4087a4a9a0e23d7dc960cadb0a6
suren <surenpal666@gmail.com>
2026-02-17 22:31:46 +0900

    idk

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -14,8 +14,10 @@ const Carousel = () => {
   }, [])
 
   var settings = {
-    dots: true,
+    dots: false,
     infinite: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -24,24 +26,27 @@ const Carousel = () => {
   return (
     <div>
       <Slider {...settings}>
-{
-  data?Slice(0,7)?.map((item, index) => {
-    return <div key={index} className='bg-gradient-to-r from [#0f0c29] via-to-[324243e] -z-10'>
-    <div className='flex gap-10 justify-center h-[600px] items-center px-4'>
-      <div className='space-y-6'>
-        <h3 className='text-red-500 font-semibold font-sans text-sm'>Lorem ipsum, dolor sit amet 
-          consectetur adipisicing elit. Veritatis nihil sint, quasi qui esse dicta ducimus eaque, 
-          sit numquam a tempora incidunt? Culpa cumque labore ab eum a error tempore.</h3>
-        <h1 className='text-4xl font-bold  uppercase line-clam-3 md:w-[500px] text-white'>{item.title}</h1>
-        <p className = 'md:w-[500px] line-clamp-3 text-gray-300 pr-7'>{item.description}</p>
-        <button className='bg-gradient-to-r from-red-500 to-purple-500 text-white px-3 py-2 
-        rounded-md cursor-pointer mt-2'>Shop Now</button>
+        {
+          data?.slice(0,7)?.map((item, index) => {
+            return <div key={index} className='bg-gradient-to-r from-[#0f0c29] via-[#302b63] to-[#24243e] -z-10'>
+          <div className='flex gap-10 justify-center h-[600px] items-center px-4'>
+            <div className='space-y-6'>
+              <h3 className='text-red-500 font-semibold font-sans text-sm'>Lorem ipsum, dolor sit amet
+                consectetur adipisicing elit. Veritatis nihil sint, quasi qui esse .</h3>
+              <h1 className='text-4xl font-bold  uppercase line-clam-3 md:w-[500px] text-white'>{item.title}</h1>
+              <p className='md:w-[500px] line-clamp-3 text-gray-300 pr-7'>{item.description}</p>
+              <button className='bg-gradient-to-r from-red-500 to-purple-500 text-white px-3 py-2 
+              rounded-md cursor-pointer mt-2'>Shop Now
+              </button>
+            </div>
+          </div>
+          <img src={item.image} alt={item.title} className='rounded-full w-[550px]
+          hover:scale-105 transition-all shadow-red-400S'/>
+        </div>
+        <div>
       </div>
-    </div>
-  })
-}
-
-        
+          })
+        }
         <div>
           <h3>1</h3>
         </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import Carousel from '../components/Carousel'
+import { useContext } from 'react'
+import { DataContext } from '../context/DataContext'    
+
 
 const Home = () => {
   return (

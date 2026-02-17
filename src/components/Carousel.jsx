92f6e40c203936108874807c54a40fa4062e3b8f
suren <surenpal666@gmail.com>
2026-02-17 21:41:41 +0900

    inserting the data for the carosel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -24,23 +24,26 @@ const Carousel = () => {
   return (
     <div>
       <Slider {...settings}>
+{
+  data?Slice(0,7)?.map((item, index) => {
+    return <div key={index} className='bg-gradient-to-r from [#0f0c29] via-to-[324243e] -z-10'>
+    <div className='flex gap-10 justify-center h-[600px] items-center px-4'>
+      <div className='space-y-6'>
+        <h3 className='text-red-500 font-semibold font-sans text-sm'>Lorem ipsum, dolor sit amet 
+          consectetur adipisicing elit. Veritatis nihil sint, quasi qui esse dicta ducimus eaque, 
+          sit numquam a tempora incidunt? Culpa cumque labore ab eum a error tempore.</h3>
+        <h1 className='text-4xl font-bold  uppercase line-clam-3 md:w-[500px] text-white'>{item.title}</h1>
+        <p className = 'md:w-[500px] line-clamp-3 text-gray-300 pr-7'>{item.description}</p>
+        <button className='bg-gradient-to-r from-red-500 to-purple-500 text-white px-3 py-2 
+        rounded-md cursor-pointer mt-2'>Shop Now</button>
+      </div>
+    </div>
+  })
+}
+
+        
         <div>
           <h3>1</h3>
-        </div>
-        <div>
-          <h3>2</h3>
-        </div>
-        <div>
-          <h3>3</h3>
-        </div>
-        <div>
-          <h3>4</h3>
-        </div>
-        <div>
-          <h3>5</h3>
-        </div>
-        <div>
-          <h3>6</h3>
         </div>
       </Slider>
     </div>

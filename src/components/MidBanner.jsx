5bb481ec28c0d2307f958c5df9d0d07f20dfcdc8
suren <surenpal666@gmail.com>
2026-02-20 23:51:58 +0900

    going to impor tthe image

--- src/components/MidBanner.jsx
+++ src/components/MidBanner.jsx
@@ -1,14 +1,16 @@
+
+
 const MidBanner = () => {
   return (
-    <div className="bg-gray-100 md:py-24">
+    <div className="bg-gray-100">
       <div
-        className="relative max-w-7xl mx-auto md:rounded-2xl pt-28 bg-cover bg-center h[500vh]"
+        className="relative max-w-7xl mx-auto pt-48 bg-cover bg-center h-[700px]"
         style={{
           backgroundImage:
-            "url('https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80')",
+            "url('../assets/mid-banner.jpg')",
         }}
       >
-        <div className="absolute inset-0 bg-gradient-to-t from-black/70 to-transparent rounded-2xl"></div>
+        <div className="absolute inset-0 bg-gradient-to-t from-black/70 to-transparent"></div>
 
         <div className="relative z-10 text-center text-white px-4">
           <h2 className="text-3xl md:text-5xl font-bold mb-4">Discover Your Style</h2>

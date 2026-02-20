ec3dcb8ff51c4246c5a46df6bc28ab8f7f7e0101
suren <surenpal666@gmail.com>
2026-02-20 23:38:55 +0900

    height changed to 500 vh

--- src/components/MidBanner.jsx
+++ src/components/MidBanner.jsx
@@ -2,7 +2,7 @@ const MidBanner = () => {
   return (
     <div className="bg-gray-100 md:py-24">
       <div
-        className="relative max-w-7xl mx-auto md:rounded-2xl pt-28 bg-cover bg-center"
+        className="relative max-w-7xl mx-auto md:rounded-2xl pt-28 bg-cover bg-center h[500vh]"
         style={{
           backgroundImage:
             "url('https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80')",

8fcb4cd63a8d7c2ed713ec533451f3384e1e4f6c
suren <surenpal666@gmail.com>
2026-02-26 22:37:47 +0900

    adding the elements to the features component

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 
+const Features = [
+    {icon: Truck, text: "Free Shipping", subtext: "On orders over $50"},
+    {icon: ShieldCheck, text: "Secure Payment", subtext: "100% secure payment"},
+    {icon: Refresh, text: "Easy Returns", subtext: "30-day return policy"},
+    {icon: Support, text: "24/7 Support", subtext: "We're here to help"},
+]
+
 const Features = () => {
   return (
-    <div>Features</div>
+    <div className='py-8 px-4 sm:px-6lg:px-8'>Features</div>
   )
 }
 

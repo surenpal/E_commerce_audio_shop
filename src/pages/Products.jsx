a6751620df6ae0ca51ca4898d21c6cc4d43afdbf
suren <surenpal666@gmail.com>
2026-02-27 22:11:53 +0900

    product will be displayed here

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { getData } from '../context/DataContext'
 
 const Products = () => {
+
+  const { data} = getData()
   return (
-    <div>Products</div>
+    <div>
+      <div>
+        {
+          data.length > 0 ? (
+            <div></div>
+            
+          ):(
+            <div></div>
+          )
+        }
+      </div>
+    </div>
   )
 }
 

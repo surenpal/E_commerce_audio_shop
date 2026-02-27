0d9c09c95fbee24675e4c5340daa633a66a90267
suren <surenpal666@gmail.com>
2026-02-27 21:50:00 +0900

    elements for the footer is recorrected

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,13 +14,13 @@ const Footer = () => {
                         </h1>
                     </Link>
                     <p className="mt-3 text-sm">
-                        Powering Your World with the Best in Electronics.
+                        The one and only place, where you can find the best beauty, electronic, Home Decor products, with the best prices. We are committed to providing you with an exceptional shopping experience and top-notch customer service.
                     </p>
 
                     <div className="mt-4 space-y-1 text-sm">
-                        <p>123 Electronics St, Style City, NY 10001</p>
+                        <p>Tokyo,Adachi-Ku,1210823,New street</p>
                         <p>Email: support@MELA.com</p>
-                        <p>Phone: (123) 456-7890</p>
+                        <p>Phone: (030) 4456-6745</p>
                     </div>
                 </div>
 
@@ -51,7 +51,7 @@ const Footer = () => {
                 <div>
                     <h3 className="text-lg font-semibold text-white mb-3">Stay in the Loop</h3>
                     <p className="text-sm mb-4">
-                        Subscribe to get special offers, free giveaways, and more.
+                        Subscribe to get special offers, and more.
                     </p>
 
                     <div className="flex">

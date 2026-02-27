6f74e0883b0538f458fed1d0d9dea5910dc978bd
suren <surenpal666@gmail.com>
2026-02-28 00:28:23 +0900

    next session for the product page in the e-commerce websites

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -13,7 +13,15 @@ const Products = () => {
       <div>
         {
           data.length > 0 ? (
-            <div>product 1</div>
+            <div>
+              <h1 className='text-3xl font-bold mb-4'>
+                {data[0].title}
+              </h1>
+              <p className='text-gray-700 mb-6'>
+                {data[0].description}
+              </p>
+              <img src="" alt="" />
+            </div>
             
           ):(
             <div>No products available</div>

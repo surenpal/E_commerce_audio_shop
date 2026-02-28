dbcfa4e04c630be5966b0011f3815765a96b082b
suren <surenpal666@gmail.com>
2026-02-28 23:40:59 +0900

    Filtersection is creating , to filter the products here

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -14,15 +14,7 @@ const Products = () => {
         {
           data.length > 0 ? (
             <div>
-              <h1 className='text-3xl font-bold mb-4'>
-                {data[0].title}
-              </h1>
-              <p className='text-gray-700 mb-6'>
-                {data[0].description}
-              </p>
-              <img src="" alt="" />
-
-              <h3 className=''>Shop Now</h3>
+              <FilterSection/>
             </div>
             
             
